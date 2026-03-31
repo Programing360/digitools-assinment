7ed744ed1c88502ea3901a405b2132a2540b4dd3
Md Limon <fhlimon360@gmail.com>
2026-03-31 12:12:22 +0600

    added cart item

--- src/components/Root/Cart/Cart.jsx
+++ src/components/Root/Cart/Cart.jsx
@@ -15,45 +15,7 @@ const Cart = ({ cartItems, setCartItems,setCount }) => {
   }
   return (
     <div className="min-h-screen">
-      <h1 className="text-3xl font-bold mb-5">Your Cart</h1>
-      {cartItems.length === 0 ? (
-        <p className="text-gray-500 text-center">Your cart is empty.</p>
-      ) : (
-        <div>
-          {cartItems.map((cart) => (
-            <div
-              key={cart.id}
-              className="border border-gray-400 p-4 rounded-lg mb-4 flex justify-between items-center"
-            >
-              <div>
-                <h2 className="text-[20px] font-bold">{cart.name}</h2>
-                <p>{cart.description}</p>
-                <p>${cart.price.toFixed(2)}</p>
-              </div>
-              <div>
-                <button
-                  onClick={() => handleRemove(cart.id)}
-                  className="text-red-500 cursor-pointer active:scale-95"
-                >
-                  Remove
-                </button>
-              </div>
-            </div>
-          ))}
-          <div className="flex justify-between items-center">
-            <h2 className="text-xl font-bold">Total:</h2>
-            <p>
-              $
-              {cartItems
-                .reduce((total, item) => total + item.price, 0)
-                .toFixed(2)}
-            </p>
-          </div>
-          <button onClick={handleRemoveAll} className="bg-linear-to-r from-blue-500 to-purple-600 text-white w-full py-2 rounded-full mt-5 mb-30 cursor-pointer active:scale-95">
-            Process to Checkout
-          </button>
-        </div>
-      )}
+      
     </div>
   );
 };
